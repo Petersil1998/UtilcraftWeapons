57a99432c86ed914536417640d9983bd968d146a
Petersil1998 <silbernagl.peter@rolmail.net>
2021-06-27 15:07:30 +0200

    Added lang entries for smoke grenade item and itemGroup

--- src/main/java/net/petersil98/utilcraft_weapons/datagen/Languages.java
+++ src/main/java/net/petersil98/utilcraft_weapons/datagen/Languages.java
@@ -2,17 +2,21 @@ package net.petersil98.utilcraft_weapons.datagen;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
-import net.petersil98.utilcraft.items.ModItems;
 import net.petersil98.utilcraft_weapons.UtilcraftWeapons;
 import net.petersil98.utilcraft_weapons.effects.UtilcraftWeaponsEffects;
 import net.petersil98.utilcraft_weapons.items.UtilcraftWeaponsItems;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 
 public class Languages {
 
+    @Nonnull
     public static English getEnglish(DataGenerator generator){
         return new English(generator);
     }
 
+    @Nonnull
     public static German getGerman(DataGenerator generator){
         return new German(generator);
     }
@@ -29,6 +33,10 @@ public class Languages {
             add(UtilcraftWeaponsItems.SNIPER_RIFLE, "Sniper Rifle");
             add(UtilcraftWeaponsEffects.STEALTH, "Stealth");
             add(UtilcraftWeaponsItems.BULLET_ITEM,"Bullet");
+            add(UtilcraftWeaponsItems.SMOKE_GRENADE, "Smoke Grenade");
+
+            add(String.format("itemGroup.%s", UtilcraftWeapons.MOD_ID), "Utilcraft Weapons");
+
             add(String.format("%s.kill.sniper", UtilcraftWeapons.MOD_ID), "%s got sniped by %s");
             add(String.format("%s.kill.knife", UtilcraftWeapons.MOD_ID), "%s got stabbed by %s");
         }
